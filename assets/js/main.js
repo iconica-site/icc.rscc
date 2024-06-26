579069ca0ae06a1b6cc855231e1e9878be904bfa
Eugene <yousucker8@gmail.com>
2024-06-26 11:36:38 +0300

    version 2.1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,6 @@
 
 
 
-// let burgerBlock = document.querySelector('.adaptiveNavBlock');
-// let button = document.querySelector('.adaptiveNavBurgerBtn');
-// let listA = document.querySelectorAll('.adaptiveNavBlock a');
-
-// let stick1 = document.querySelector('.stick1');
-// let stick2 = document.querySelector('.stick2');
-// let stick3 = document.querySelector('.stick3');
-
-// let body = document.querySelector('body');
-
-// button.addEventListener('click', function() {
-//     if (burgerBlock.style.display === "flex") {
-//         burgerBlock.style.display = "none";
-//         body.style.overflow = "auto"
-//         listA.forEach(function(item) {
-//             item.style.display = "none";
-//         });
-
-//         stick1.style.transform = "";
-//         stick1.style.top = "";
-//         stick2.style.display = "";
-//         stick3.style.transform = "";
-//     } else {
-//         burgerBlock.style.display = "flex";
-//         body.style.overflow = "hidden"
-//         listA.forEach(function(item) {
-//             item.style.display = "flex";
-//         });
-
-//         stick1.style.transform = "rotateZ(-40deg)";
-//         stick1.style.top = "7px"
-
-
-
-//         stick2.style.display = "none";
-//         stick3.style.transform = "rotateZ(40deg)";
-        
-//     }
-// });
-
-
-
-
 
 let burgerBlock = document.querySelector('.adaptiveNavBlock');
 let button = document.querySelector('.adaptiveNavBurgerBtn');
